c2acc9cf65f7131db22adb577fc2ed5ef218e04e
Daniele Barell <dan.barell@gmail.com>
2025-05-29 12:03:10 +0200

    Chp 15 Authenication
    This tutorial was very unclear.
    Anyway it's a building up of authentication and authorization mechanism with some third parties libs

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,8 @@ import { z } from "zod";
 import postgres from "postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 
 export type State = {
   message?: string | null;
@@ -110,4 +112,22 @@ export async function deleteInvoiceAction(id: string) {
   WHERE id=${id}
   `;
   revalidatePath(redirectURL); //remove cash so invoices page will be refreshed
+}
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
 }
